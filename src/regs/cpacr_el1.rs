b2232ad96c5e5cc2b2b35a007b627967fcf8aa67
Brian Howard <bhoward@depauw.edu>
2018-07-20 22:41:53 -0400

    Added partial CPACR_EL1

---
+++ src/regs/cpacr_el1.rs
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2018 by the author(s)
+ *
+ * =============================================================================
+ *
+ * Licensed under either of
+ *   - Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ *   - MIT License (http://opensource.org/licenses/MIT)
+ * at your option.
+ *
+ * =============================================================================
+ *
+ * Author(s):
+ *   - Andre Richter <andre.o.richter@gmail.com>
+ *   - Brian Howard <bhoward@depauw.edu>
+ */
+
+//! Architectural Feature Access Control register - EL2
+
+pub use register::cpu::RegisterReadWrite;
+
+register_bitfields! {u32,
+    CPACR_EL1 [
+        /// TODO write more here
+        FPEN           OFFSET(20)  NUMBITS(2) [
+            Enable = 3
+        ]
+    ]
+}
+
+pub struct Reg;
+
+impl RegisterReadWrite<u32, CPACR_EL1::Register> for Reg {
+    sys_coproc_read_raw!(u32, "CPACR_EL1");
+    sys_coproc_write_raw!(u32, "CPACR_EL1");
+}
+
+pub static CPACR_EL1: Reg = Reg {};
--- src/regs/mod.rs
+++ src/regs/mod.rs
@@ -8,6 +8,7 @@ pub mod cnthctl_el2;
 pub mod cntp_ctl_el0;
 pub mod cntp_tval_el0;
 pub mod cntvoff_el2;
+pub mod cpacr_el1;
 pub mod currentel;
 pub mod hcr_el2;
 pub mod mpidr_el1;
